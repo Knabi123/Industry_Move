31a6c29e579a58d72637697a4e553099bd12fcd2
KSD <kitsada.wa@ku.th>
2024-01-26 03:36:19 +0700

    ksd

--- lib/src/driverscreen.dart
+++ lib/src/driverscreen.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: use_key_in_widget_constructors, library_private_types_in_public_api, prefer_const_constructors, non_constant_identifier_names, avoid_types_as_parameter_names
+
 import 'package:flutter/material.dart';
 import 'dart:io';
 import 'package:company/utils/add_driver.dart';
