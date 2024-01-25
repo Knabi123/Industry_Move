5b716ebdf400260a2bb236a984b143ed55aa367a
KSD <kitsada.wa@ku.th>
2024-01-26 02:45:34 +0700

    ksd

--- lib/src_user/login.dart
+++ lib/src_user/login.dart
@@ -59,6 +59,7 @@ class _LoginPageState extends State<LoginPage> {
                   Text(
                     "Login",
                     style: TextStyle(
+                        fontFamily: 'Kanit-Black',
                         color: Color.fromRGBO(49, 39, 79, 1),
                         fontWeight: FontWeight.bold,
                         fontSize: 40),
